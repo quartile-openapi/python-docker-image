b7bf6ca29440f3f15b7b10b496d047d04d0299e4
carlos-rian <crian.rian@gmail.com>
2023-08-02 12:38:42 -0300

    update docker image for use the latest

--- readme.md
+++ readme.md
@@ -1,3 +1,23 @@
-# Python 3.10.1
+# Quartile Docker Image
 
-## [Python Lib](requirements.txt)
+This project builds a Python docker image as a base for Quartile projects.
+
+
+## Development
+
+### Requirements
+
+* Python 3.10
+* Poetry
+
+
+### Update your project
+
+The `update_toml.py` script will update the `pyproject.toml` file with the latest.
+This guarantees that the docker image will be built with the latest version of the Quartile library.
+
+#### Run the script in your project
+
+```bash
+python3 update_toml.py
+```
--- update_toml.py
+++ update_toml.py
@@ -1,6 +1,9 @@
+import subprocess
+
 import httpx
 from tomlkit import dumps, parse
 
+print("Updating pyproject.toml")
 new_toml = httpx.get("https://raw.githubusercontent.com/quartile-openapi/python-docker-image/main/pyproject.toml").text
 with open("pyproject.toml", "r") as f:
     old_toml = f.read()
@@ -8,6 +11,7 @@ with open("pyproject.toml", "r") as f:
 new_toml = parse(new_toml)
 old_toml = parse(old_toml)
 
+print("Updating library version")
 deps = new_toml["tool"]["poetry"]["group"]
 # [tool.poetry.group.docker.dependencies]
 old_toml["tool"]["poetry"]["group"]["docker"]["dependencies"] = deps["docker"]["dependencies"]
@@ -18,5 +22,18 @@ old_toml["tool"]["poetry"]["group"]["publish"]["dependencies"] = deps["publish"]
 # [tool.poetry.group.others.dependencies]
 old_toml["tool"]["poetry"]["group"]["others"]["dependencies"] = deps["others"]["dependencies"]
 
+print("Changing the pyproject.toml file")
 with open("pyproject.toml", "w") as f:
     f.write(dumps(old_toml))
+
+
+# remove poetry lock
+print("Removing poetry.lock")
+subprocess.run(["rm", "-rf", "poetry.lock"], check=True)
+
+# update poetry
+print("Updating poetry")
+subprocess.run(["poetry", "update"], check=True)
+
+
+print("Done")
